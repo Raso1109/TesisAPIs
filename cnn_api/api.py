eddc89159dcec5eebbb4b6cd61af711bd193cec4
Rafael Antonio Sotelo Oviedo <rafael72710261@gmail.com>
2025-06-29 00:51:37 -0500

    wa

--- cnn_api/api.py
+++ cnn_api/api.py
@@ -1,16 +1,50 @@
+import os
+import gdown
+
+MODEL_PATH = "parkinson_spiral_cnn_82_f1.keras"
+
+if not os.path.exists(MODEL_PATH):
+    url = "https://drive.google.com/file/d/1oY_A1YfelAj24jqOtsBSPqdWhfWyst_9/view?usp=sharing"
+    gdown.download(url, MODEL_PATH, quiet=False)
+
 from fastapi import FastAPI
-from pydantic import BaseModel
 import tensorflow as tf
-import numpy as np
 
 app = FastAPI()
-model = tf.keras.models.load_model("parkinson_spiral_cnn_82_f1.keras")
+model = tf.keras.models.load_model(MODEL_PATH)
 
-class ImageData(BaseModel):
-    image: list
+def predict_image(image_path, model):
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    if img is None:
+        return {"error": "Could not read image"}
 
+    img = cv2.resize(img, (128, 128))
+    img = img.astype('float32') / 255.0
+    img = np.expand_dims(img, axis=0)
+    img = np.expand_dims(img, axis=-1)
+
+    prediction = model.predict(img)
+    probabilities = prediction[0].tolist()  # Devuelve las probabilidades directamente
+
+    return {"probabilities": probabilities}
+
+
+# Endpoint para subir y predecir una imagen
 @app.post("/predict")
-def predict(data: ImageData):
-    image_array = np.array(data.image).reshape((1, 224, 224, 3))  # Ajusta al tamaño esperado por el modelo
-    prediction = model.predict(image_array).tolist()
-    return {"prediction": prediction}
+async def predict(file: UploadFile = File(...)):
+    try:
+        # Guarda temporalmente la imagen
+        temp_image_path = f"temp_{file.filename}"
+        with open(temp_image_path, "wb") as temp_file:
+            temp_file.write(await file.read())
+
+        # Realiza la predicción
+        result = predict_image(temp_image_path, model)
+
+        # Elimina el archivo temporal
+        os.remove(temp_image_path)
+
+        return result
+
+    except Exception as e:
+        return {"error": str(e)}
